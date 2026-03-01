e16e5070e06173e272a9e82db17abca835dc66b6
ST10508068 <ST10508068@myemeris.edu.za>
2026-03-01 15:20:03 +0200

    First submission

---
+++ src/main/java/com/mycompany/tableandchairsactiv/TableandChairsactiv.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tableandchairsactiv;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class TableandChairsactiv {
+
+    public static void main(String[] args) {
+        System.out.println("  X                                           X       ");
+        System.out.println("  X                                           X       ");
+        System.out.println("  X                                           X       ");
+        System.out.println("  X                                           X       ");
+        System.out.println("  X             X X X X X X X X X             X       ");
+        System.out.println("  X X X X X     X               X     X X X X X       ");
+        System.out.println("  X       X     X      JAVA     X     X       X       ");
+        System.out.println("  X       X     X               X     X       X       ");
+        System.out.println("  X       X     X               X     X       X       ");
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
