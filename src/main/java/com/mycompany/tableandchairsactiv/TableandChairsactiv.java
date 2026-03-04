d31c7949d0b1bcb41a31c311b34317a6f2b0b02f
ST10508068 <ST10508068@myemeris.edu.za>
2026-03-04 09:01:27 +0200

    Update TableandChairsactiv.java

--- src/main/java/com/mycompany/tableandchairsactiv/TableandChairsactiv.java
+++ src/main/java/com/mycompany/tableandchairsactiv/TableandChairsactiv.java
@@ -13,8 +13,8 @@ public class TableandChairsactiv {
     public static void main(String[] args) {
         System.out.println("  X                                           X       ");
         System.out.println("  X                                           X       ");
-        System.out.println("  X                                           X       ");
-        System.out.println("  X                                           X       ");
+        System.out.println("  X                  *******                  X       ");
+        System.out.println("  X                     |                     X       ");
         System.out.println("  X             X X X X X X X X X             X       ");
         System.out.println("  X X X X X     X               X     X X X X X       ");
         System.out.println("  X       X     X      JAVA     X     X       X       ");
